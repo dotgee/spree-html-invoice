cb5718721dc0064c3c477281ee16bb01cdd5bb00
Antoine tanguy <antoine.tanguy35@gmail.com>
2012-11-20 17:03:07 +0100

    conditional show link

--- app/controllers/spree/invoice_controller.rb
+++ app/controllers/spree/invoice_controller.rb
@@ -7,6 +7,10 @@ module Spree
       template = params[:template] 
       @order = Spree::Order.find_by_number(params[:id])
       @order ||= current_order
+      if @order.payment.nil? || !@order.payment.completed?
+        flash[:notice] = I18n.(:invoice_not_printable).html_safe
+        return redirect_to account_path 
+      end
       # if @order && ( @order.user == try_spree_current_user || @order == current_order) && @order.state == 'completed'
       if @order && ( @order.user == try_spree_current_user || @order == current_order) && @order.state == 'complete'
         render "spree/invoice/#{template}" , :layout => false and return
