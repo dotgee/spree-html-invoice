05a8035f0241d1db5cffb31e008612cbcd2f2484
=atanguy <=atanguy@dotgee.fr>
2012-12-18 16:38:11 +0100

    ajout facture en admin

--- app/overrides/add_html_invoice_buttons.rb
+++ app/overrides/add_html_invoice_buttons.rb
@@ -3,8 +3,8 @@ Deface::Override.new(:virtual_path => "spree/orders/show",
                      :insert_bottom => "#order_summary legend[1]",
                      :partial => "spree/invoice/print_link")
 
-Deface::Override.new(:virtual_path => "spree/admin/shared/_order_tabs",
-                     :name => "add_html_invoice_buttons",
-                     :insert_after => ".sidebar",
+Deface::Override.new(:virtual_path => "spree/admin/orders/show",
+                     :name => "print_buttons",
+                     :insert_bottom => "[data-hook='admin_order_show_buttons'], #admin_order_show_buttons[data-hook]",
                      :partial => "spree/admin/orders/html_buttons",
                      :disabled => false)
