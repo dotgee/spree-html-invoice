bc43065f5ecbad75d5155cb9996c1a7611c1be7d
johann@dotgee.fr <johann@dotgee.fr>
2013-01-30 10:37:15 +0100

    Print button on spree 1.3

--- app/overrides/add_html_invoice_buttons.rb
+++ app/overrides/add_html_invoice_buttons.rb
@@ -3,8 +3,14 @@ Deface::Override.new(:virtual_path => "spree/orders/show",
                      :insert_bottom => "#order_summary legend[1]",
                      :partial => "spree/invoice/print_link")
 
+#Deface::Override.new(:virtual_path => "spree/admin/orders/show",
+#                     :name => "print_buttons",
+#                     :insert_bottom => "[data-hook='admin_order_show_buttons'], #admin_order_show_buttons[data-hook]",
+#                     :partial => "spree/admin/orders/html_buttons",
+#                     :disabled => false)
+# Spree 1.3
 Deface::Override.new(:virtual_path => "spree/admin/orders/show",
                      :name => "print_buttons",
-                     :insert_bottom => "[data-hook='admin_order_show_buttons'], #admin_order_show_buttons[data-hook]",
+                     :insert_after => "code[erb-silent]:contains('content_for :page_actions do')",
                      :partial => "spree/admin/orders/html_buttons",
                      :disabled => false)
